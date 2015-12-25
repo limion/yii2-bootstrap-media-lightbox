035879f8ce1d0d659d4523aa27abe423d54ff3ec
limion <vlad.holovko@gmail.com>
2015-12-25 12:22:09 +0200

    Create BootstrapMediaLightboxAsset.php

---
+++ BootstrapMediaLightboxAsset.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace limion\bootstrap-media-lightbox;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Vladyslav Holovko <vlad.holovko@gmail.com>
+ */
+class BootstrapMediaLightboxAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/bootstrap-plus/bootstrap-media-lightbox';
+    public $css = ['bootstrap-media-lightbox.css'];
+    public $js = ['bootstrap-media-lightbox.min.js'];
+    public $depends = ['yii\web\JqueryAsset'];
+}
